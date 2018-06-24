f35f57561064941b1233df7dfb53e93ff4aed02a
SaahilNZ <saahilh@outlook.co.nz>
2018-06-24 21:09:36 +1200

    Import vertex buffer file as a byte array.

--- blender/import_destiny_item.py
+++ blender/import_destiny_item.py
@@ -1,6 +1,7 @@
 import bpy
 import bmesh
 
+
 def convert_to_tri_list(tri_strip):
     tri_list = [(tri_strip[0], tri_strip[2], tri_strip[1])]
     for i in range(1, len(tri_strip) - 2):
@@ -11,51 +12,107 @@ def convert_to_tri_list(tri_strip):
     return tri_list
 
 
-verts = [
-    (0,0,0),
-    (0,1,0),
-    (1,0,0),
-    (1,1,0),
-    (2,0,0),
-    (2,1,0),
-    (3,0,0),
-    (3,1,0),
-    (0,0,1),
-    (0,1,1),
-    (1,0,1),
-    (1,1,1),
-    (2,0,1),
-    (2,1,1),
-    (3,0,1),
-    (3,1,1)
-]
+def read_some_data(context, filepath):
+    f = open(filepath, 'rb')
+    data = bytearray(f.read())
+    f.close()
 
-tri_strip = [0,1,2,3,4,5,6,7]
-indices = convert_to_tri_list(tri_strip)
+    verts = [
+        (0,0,0),
+        (0,1,0),
+        (1,0,0),
+        (1,1,0),
+        (2,0,0),
+        (2,1,0),
+        (3,0,0),
+        (3,1,0),
+        (0,0,1),
+        (0,1,1),
+        (1,0,1),
+        (1,1,1),
+        (2,0,1),
+        (2,1,1),
+        (3,0,1),
+        (3,1,1)
+    ]
 
-mesh = bpy.data.meshes.new("MyMesh")
-obj = bpy.data.objects.new("MyObject", mesh)
+    tri_strip = [0,1,2,3,4,5,6,7]
+    indices = convert_to_tri_list(tri_strip)
 
-scene = bpy.context.scene
-scene.objects.link(obj)
-scene.objects.active = obj
+    mesh = bpy.data.meshes.new("MyMesh")
+    obj = bpy.data.objects.new("MyObject", mesh)
 
-mesh = bpy.context.object.data
-bm = bmesh.new()
+    scene = bpy.context.scene
+    scene.objects.link(obj)
+    scene.objects.active = obj
 
-for v in verts:
-    bm.verts.new(v)
+    mesh = bpy.context.object.data
+    bm = bmesh.new()
 
-bm.verts.ensure_lookup_table()
-for i in indices:
-    face = (bm.verts[i[0]], bm.verts[i[1]], bm.verts[i[2]])
-    bm.faces.new(face)
+    for v in verts:
+        bm.verts.new(v)
 
-bm.to_mesh(mesh)
-bm.free()
+    bm.verts.ensure_lookup_table()
+    for i in indices:
+        face = (bm.verts[i[0]], bm.verts[i[1]], bm.verts[i[2]])
+        bm.faces.new(face)
 
-bpy.ops.object.mode_set(mode='EDIT')
-for v in bmesh.from_edit_mesh(mesh).verts:
-    v.select = True
-bpy.ops.mesh.delete_loose()
-bpy.ops.object.mode_set(mode='OBJECT')
+    bm.to_mesh(mesh)
+    bm.free()
+
+    bpy.ops.object.mode_set(mode='EDIT')
+    for v in bmesh.from_edit_mesh(mesh).verts:
+        v.select = True
+    bpy.ops.mesh.delete_loose()
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+    print(len(data))
+
+    return {'FINISHED'}
+
+
+# ImportHelper is a helper class, defines filename and
+# invoke() function which calls the file selector.
+from bpy_extras.io_utils import ImportHelper
+from bpy.props import StringProperty, BoolProperty, EnumProperty
+from bpy.types import Operator
+
+
+class ImportDestinyItem(Operator, ImportHelper):
+    """This appears in the tooltip of the operator and in the generated docs"""
+    bl_idname = "import_data.destiny_item"  # important since its how bpy.ops.import_test.some_data is constructed
+    bl_label = "Import Destiny Item"
+
+    # ImportHelper mixin class uses this
+    filename_ext = ".tgx"
+
+    filter_glob = StringProperty(
+            default="*.tgx",
+            options={'HIDDEN'},
+            maxlen=255,  # Max internal buffer length, longer would be clamped.
+            )
+
+    def execute(self, context):
+        return read_some_data(context, self.filepath)
+
+
+# Only needed if you want to add into a dynamic menu
+def menu_func_import(self, context):
+    self.layout.operator(ImportDestinyItem.bl_idname, text="Import Destiny Item")
+
+
+def register():
+    bpy.utils.register_class(ImportDestinyItem)
+    bpy.types.INFO_MT_file_import.append(menu_func_import)
+
+
+def unregister():
+    bpy.utils.unregister_class(ImportDestinyItem)
+    bpy.types.INFO_MT_file_import.remove(menu_func_import)
+
+
+if __name__ == "__main__":
+    register()
+
+    # test call
+    bpy.ops.import_data.destiny_item('INVOKE_DEFAULT')
