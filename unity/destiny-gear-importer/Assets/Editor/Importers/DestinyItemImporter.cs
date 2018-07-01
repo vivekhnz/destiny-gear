4ce7e2576b16ee28ea8a1128ee413c4388514fa9
Vivek Hari <vivek.hari@outlook.co.nz>
2018-07-01 21:30:49 +1200

    Add basic scripted importer for .meshes file.

---
+++ unity/destiny-gear-importer/Assets/Editor/Importers/DestinyItemImporter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor.Experimental.AssetImporters;
+using System.IO;
+
+[ScriptedImporter(1, "meshes")]
+public class DestinyItemImporter : ScriptedImporter
+{
+    public override void OnImportAsset(AssetImportContext ctx)
+    {
+        using (var stream = new FileStream(ctx.assetPath, FileMode.OpenOrCreate))
+        {
+            using (var reader = new BinaryReader(stream))
+            {
+                int arrangementCount = reader.ReadInt32();
+                Debug.Log(arrangementCount);
+            }
+        }
+
+        var material = new Material(Shader.Find("Standard"));
+        material.color = Color.red;
+        ctx.AddObjectToAsset("material", material);
+
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = new Vector3(0, 0, 0);
+        cube.GetComponent<MeshRenderer>().material = material;
+
+        ctx.AddObjectToAsset("root", cube);
+        ctx.SetMainObject(cube);
+    }
+}
